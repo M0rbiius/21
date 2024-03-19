84bc9984950662643eb5783132d80c1730774b74
M0rbiius <106761547+M0rbiius@users.noreply.github.com>
2024-03-19 17:49:38 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,65 @@
-
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
-
-
-let messageEl = document.getElementById("message-el")
-
-
-let sumEl= document.getElementById("sum-el")
-//let sumEl = document.querySelector("body")
-
-let cardEl = document.getElementById("card-el")
-
-function startGame(){
-rendertGame()
+let player = {
+    name: "Per",
+    chips: 200
 }
-function rendertGame() {
 
-    cardEl.textContent = "Cards:" + randomNumber1 +" " + randomNumber2;
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
 
-    sumEl.textContent = "Sum:" + ranSum;
+playerEl.textContent = player.name + ": $" + player.chips
 
-    if (ranSum <= 20) {
-        message = "Do you want to draw a new card? "
-    } else if (ranSum === 21) {
-        message = "You've got Blackjack! "
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! "
+        message = "You're out of the game!"
         isAlive = false
     }
-   
-     messageEl.textContent = message;
+    messageEl.textContent = message
 }
 
 
 function newCard() {
-   console.log("Drawing a new card from the deck!")
-
-   
-   ranSum += randomNumber3;
-  rendertGame()
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
 }
-
-
-
-
-// Генерация случайного числа в диапазоне от min (включительно) до max (включительно)
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// Получение двух разных случайных чисел
-let randomNumber1 = getRandomNumber(1, 10); 
-let randomNumber2 = getRandomNumber(1, 10); 
-let randomNumber3 = getRandomNumber(1, 10);
-console.log(randomNumber3)
-ranSum = randomNumber1 + randomNumber2;
